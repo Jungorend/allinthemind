57b8755a27cdca5dcc7895f1d24b5b4312a8d2da
Jungy <jungorend@gmail.com>
2022-10-19 16:29:46 -0400

    Add palette

--- src/core.s
+++ src/core.s
@@ -4,6 +4,12 @@
 .segment "ZEROPAGE"
 player_one_buttons: .res 1
 player_two_buttons: .res 1
+player_one_x:   .res 1
+player_one_y:   .res 1
+player_one_state:   .res 1
+player_two_x:   .res 1
+player_two_y:   .res 1
+player_two_state:   .res 1
 .exportzp player_one_buttons, player_two_buttons
 
 .segment "CODE"
@@ -25,6 +31,20 @@ player_two_buttons: .res 1
 .import poll_controllers
 .export main
 .proc main
+    ;; load palette
+    LDX PPUSTATUS
+    LDX $3f
+    STX PPUADDR
+    LDX $00
+    STX PPUADDR
+palette_loop:
+    LDA palette,X
+    STA PPUDATA
+    INX
+    CMX #$10
+    BNE palette_loop
+
+
 main_loop:
     JSR poll_controllers
     JMP main_loop
@@ -35,3 +55,16 @@ main_loop:
 
 .segment "CHR"
     .res 8192
+
+.segment "RODATA"
+palette:
+    ;; backgrounds
+    .byte $0f, $09, $19, $29    ; green just for testing purposes
+    .byte $0f, $0f, $0f, $0f
+    .byte $0f, $0f, $0f, $0f
+    .byte $0f, $0f, $0f, $0f
+    ;; sprites
+    .byte $0f, $0f, $0f, $0f
+    .byte $0f, $0f, $0f, $0f
+    .byte $0f, $0f, $0f, $0f
+    .byte $0f, $0f, $0f, $0f
